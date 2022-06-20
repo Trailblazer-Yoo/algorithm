c5f10472dff01b582ec598b6d254c81079ad498c
Trailblazer-Yoo <yousun9591@naver.com>
2022-06-20 22:57:02 +0900

    2022-06-20

--- PROGRAMERS/로또의최고순위와최저순위.py
+++ PROGRAMERS/로또의최고순위와최저순위.py
@@ -12,3 +12,14 @@ def solution(lottos, win_nums):
 lottos = [45, 4, 35, 20, 3, 9]
 win_nums = [20, 9, 3, 45, 4, 35]
 print(solution(lottos, win_nums))
+
+def solution(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
--- PROGRAMERS/신규아이디추천.py
+++ PROGRAMERS/신규아이디추천.py
@@ -1,26 +1,34 @@
 import re
 
 def solution(new_id):
+    # 1단계
     new_id = new_id.lower()
-    print(new_id)
+    
+    # 2단계
     new_id = re.sub('[^a-z0-9_.-]+', '', new_id)
+    
+    # 3단계
     while True:
         if new_id == new_id.replace('..','.'):
             break
         else:
             new_id = new_id.replace('..','.')
-    print(new_id, len(new_id))
+    
+    # 4단계
     new_id = new_id.strip('.')
+    
+    # 5단계
     if not new_id:
         new_id = 'a'
+        
+    # 6단계
     if len(new_id) > 15:
         new_id = new_id[:15]
     new_id = new_id.strip('.')
-    print(new_id, len(new_id))
-    print(new_id)
+    
+    # 7단계
     while len(new_id) < 3:
         new_id += new_id[-1]
-    print(new_id)
 
     return new_id
     
